cb315d46e555dd50a60be9c184089d7b0fca1d9e
KaptSprinkles <KSoto18@users.noreply.github.com>
2022-10-27 18:09:52 -0400

    added CSS

--- main/src/page-template.js
+++ main/src/page-template.js
@@ -4,15 +4,15 @@ const generateTeam = team => {
     const generateManager = manager => {
         return `
         <div class="card m-3">
-        <div class="card-header">
-            <h2 class="card-title">${manager.getName()}</h2>
-            <h3 class="card-title">${manager.getRole()}</h3>
+        <div class="card-header bg-info text-white">
+            <h2>${manager.getName()}</h2>
+            <h3>${manager.getRole()}</h3>
         </div>
         <div class="card-body">
             <ul>
-                <li>ID: ${manager.getId()}</li>
-                <li>Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
-                <li>Office number: ${manager.getOfficeNumber()}</li>
+                <li><b>ID: </b>${manager.getId()}</li>
+                <li><b>Email: </b><a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
+                <li><b>Office Number: </b>${manager.getOfficeNumber()}</li>
             </ul>
         </div>
     </div>
@@ -23,15 +23,15 @@ const generateTeam = team => {
     const generateEngineer = engineer => {
         return `
         <div class="card m-3">
-        <div class="card-header">
-            <h2 class="card-title">${engineer.getName()}</h2>
-            <h3 class="card-title">${engineer.getRole()}</h3>
+        <div class="card-header bg-dark text-white">
+            <h2>${engineer.getName()}</h2>
+            <h3>${engineer.getRole()}</h3>
         </div>
         <div class="card-body">
             <ul>
-                <li>ID: ${engineer.getId()}</li>
-                <li>Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
-                <li>GitHub: <a href="https://github.com/${engineer.getGithub()}">${engineer.getGithub()}</a></li>
+                <li><b>ID: </b>${engineer.getId()}</li>
+                <li><b>Email: </b><a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+                <li><b>GitHub: </b><a href="https://github.com/${engineer.getGithub()}">${engineer.getGithub()}</a></li>
             </ul>
         </div>
     </div>
@@ -42,15 +42,15 @@ const generateTeam = team => {
     const generateIntern = intern => {
         return `
         <div class="card m-3">
-        <div class="card-header">
+        <div class="card-header bg-success text-white">
             <h2>${intern.getName()}</h2>
             <h3>${intern.getRole()}</h3>
         </div>
         <div class="card-body">
             <ul>
-                <li>ID: ${intern.getId()}</li>
-                <li>Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
-                <li>School: ${intern.getSchool()}</li>
+                <li><b>ID: </b> ${intern.getId()}</li>
+                <li><b>Email: </b><a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
+                <li><b>School: </b> ${intern.getSchool()}</li>
             </ul>
         </div>
      </div>
@@ -86,15 +86,15 @@ module.exports = team => {
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-        <link rel="stylesheet" href="style.css">
-        <title>My Team</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="../assets/style.css">
+        <title>My Team Builder</title>
     </head>
-    <body>
+    <body class="bg-secondary">
         <div class="container-fluid">
             <div class="row">
                 <div class="col-12 jumbotron mb-3">
-                    <h1 class="text-center">My Team</h1>
+                    <h1 class="text-center title">TEAM</h1>
                 </div>
             </div>
         </div>
